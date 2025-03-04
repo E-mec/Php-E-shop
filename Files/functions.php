cb1a2c64ea5d8f7bfb986a99f2fe2d39b175c167
Exxon Mecnix <emecnix@gmail.com>
2025-03-04 21:21:10 +0100

    File Upload, Image Compress

--- Files/functions.php
+++ Files/functions.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'Zebra_Image.php';
+
 define('BASEPATH', 'localhost:8080');
 
 $conn = new mysqli('localhost', 'root', '', 'e-shop');
@@ -141,6 +143,7 @@ function upload_images($files)
             $fileName = time() . "-" . rand(1000, 10000) . "." . $ext;
 
             $destination = "Uploads/" . $fileName;
+            $thumbnail_destination = "Uploads/thumb_" . $fileName;
 
             $res = move_uploaded_file($file['tmp_name'], $destination);
 
@@ -152,7 +155,9 @@ function upload_images($files)
                 continue;
             }
 
+            create_thumbnail($destination, $thumbnail_destination);
             $img['src'] = $destination;
+            $img['thumbnail'] = $thumbnail_destination;
 
             $uploaded_images[] = $img;
 
@@ -163,4 +168,55 @@ function upload_images($files)
     }
 
     return $uploaded_images;
+}
+
+function create_thumbnail($source, $target)
+{
+    $image = new stefangabos\Zebra_Image\Zebra_Image();
+
+    $image->auto_handle_exif_orientation = true;
+    $image->source_path = $source;
+    $image->target_path = $target;
+    $image->preserve_aspect_ratio = true;
+    $image->enlarge_smaller_images = true;
+    $image->preserve_time = true;
+    // $image->handle_exif_orientation_tag = true;
+    // $image->handle_exif_orientation_tag = true;
+
+
+    $image->jpeg_quality = get_jpeg_quality(filesize($image->source_path));
+    $width = 200;
+    $height = 200;
+
+    if(!$image->resize($width, $height, ZEBRA_IMAGE_CROP_CENTER))
+    {
+        return $image->source_path;
+    } else {
+        return $image->target_path;
+    }
+    ini_set('memory_limit', '-1');
+}
+
+function get_jpeg_quality($_size)
+{
+    $size = ($_size)/1000000;
+    $qt = 50;
+
+    if ($size > 5){
+        $qt = 10;
+    } else if ($size > 4 ) {
+        $qt = 13;
+    } else if ($size > 2) {
+        $qt = 15;
+    } else if ($size > 1 ) {
+        $qt = 17;
+    } else if ($size > 0.8 ) {
+        $qt = 50;
+    } else if ($size > 0.5 ) {
+        $qt = 80;
+    } else {
+        $qt = 90;
+    }
+
+    return $qt;
 }
